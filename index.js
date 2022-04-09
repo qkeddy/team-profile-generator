ea834ee6d30e4a38f7f0c88535a1260ee4c148df
Quin Eddy <qkeddy@gmail.com>
2022-04-09 10:32:52 -0400

    Refined class definitions and tested

---
+++ index.js
@@ -0,0 +1,21 @@
+// Require classes
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
+
+// Testing class plumbing
+const ryan = new Engineer("Ryan", 123, "ryan@me.com", "ryan@github");
+const ginsu = new Intern("Ginsu", 789, "ginsu.eddy@gmail.com", "UofW");
+const barbara = new Manager("Barbara", 456, "blmeelens@gmail.com", "212-888-7777");
+
+console.log(ryan.getGitHubUser());
+console.log(ryan.getId()); 
+
+console.log(ginsu.getSchool());
+console.log(ginsu.getId()); 
+
+console.log(barbara.getOfficeNumber());
+console.log(barbara.getId()); 
