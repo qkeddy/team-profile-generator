2db0997102271b29ebc2daeb3d4c3f24e322d678
Quin Eddy <qkeddy@gmail.com>
2022-04-09 12:29:48 -0400

    Reverted back to require. Added an approach for questions to be its own class. Also added an approach for questions to be a JSON file both using the require statement.

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-// Require various modules
+// Require classes
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+// const questions = require("./src/questions");
+const questions = require("./src/questions.json");
 
-import inquirer from "inquirer";
-import questions from "./src/Questions.js";
 
-
-// const Employee = require("./lib/Employee");
-// TODO Question - what is the difference between an import/export vs. module export & require syntax? Can we use them here?  If so, how?
-// import Engineer from "./lib/Engineer";
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
-// const Manager = require("./lib/Manager");
-
-import Employee from "./lib/Employee.js";
-import Engineer from "./lib/Engineer.js";
-// const Intern = require("./lib/Intern");
-// const Manager = require("./lib/Manager");
+// import questions from "./src/Questions.js";
 
 
 
@@ -24,16 +18,13 @@ const ryan = new Engineer("Ryan", 123, "ryan@me.com", "ryan@github");
 // const barbara = new Manager("Barbara", 456, "blmeelens@gmail.com", "212-888-7777");
 
 console.log(ryan.getGitHubUser());
-console.log(ryan.getId()); 
+console.log(ryan.getId());
 
 // console.log(ginsu.getSchool());
-// console.log(ginsu.getId()); 
+// console.log(ginsu.getId());
 
 // console.log(barbara.getOfficeNumber());
-// console.log(barbara.getId()); 
-
+// console.log(barbara.getId());
 
 // // Test inquirer
-inquirer
-    .prompt(questions)
-    .catch((err) => console.error(err));
+inquirer.prompt(questions).catch((err) => console.error(err));
--- src/Questions.js
+++ src/Questions.js
@@ -1,19 +1,23 @@
-// Array of questions for inquirer to parse. 
-export default [
-    {
-        name: "fullName",
-        message: "Please enter the employee's full name\n",
-        type: "input",
-    },
-    {
-        name: "id",
-        message: "Please enter the employee's id\n",
-        type: "input",
-    },
-    {
-        name: "email",
-        message: "Please enter the employee's email\n",
-        type: "input",
-    },
-];
+// Questions module
+const questions =
+    // Array of questions for inquirer to parse.
+    [
+        {
+            name: "fullName",
+            message: "Please enter the employee's full name\n",
+            type: "input",
+        },
+        {
+            name: "id",
+            message: "Please enter the employee's id\n",
+            type: "input",
+        },
+        {
+            name: "email",
+            message: "Please enter the employee's email\n",
+            type: "input",
+        },
+    ];
 
+
+module.exports = questions;
