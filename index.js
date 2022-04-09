ac4cce696e9dcae13d33db4b1edafb8a19b7cd5c
Quin Eddy <qkeddy@gmail.com>
2022-04-09 12:07:06 -0400

    Tested converting to import statements vs require

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-// Require classes
-const Employee = require("./lib/Employee");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+// Require various modules
+
+import inquirer from "inquirer";
+import questions from "./src/Questions.js";
+
+
+// const Employee = require("./lib/Employee");
+// TODO Question - what is the difference between an import/export vs. module export & require syntax? Can we use them here?  If so, how?
+// import Engineer from "./lib/Engineer";
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+
+import Employee from "./lib/Employee.js";
+import Engineer from "./lib/Engineer.js";
+// const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
 
 
 
 // Testing class plumbing
 const ryan = new Engineer("Ryan", 123, "ryan@me.com", "ryan@github");
-const ginsu = new Intern("Ginsu", 789, "ginsu.eddy@gmail.com", "UofW");
-const barbara = new Manager("Barbara", 456, "blmeelens@gmail.com", "212-888-7777");
+// const ginsu = new Intern("Ginsu", 789, "ginsu.eddy@gmail.com", "UofW");
+// const barbara = new Manager("Barbara", 456, "blmeelens@gmail.com", "212-888-7777");
 
 console.log(ryan.getGitHubUser());
 console.log(ryan.getId()); 
 
-console.log(ginsu.getSchool());
-console.log(ginsu.getId()); 
+// console.log(ginsu.getSchool());
+// console.log(ginsu.getId()); 
 
-console.log(barbara.getOfficeNumber());
-console.log(barbara.getId()); 
+// console.log(barbara.getOfficeNumber());
+// console.log(barbara.getId()); 
+
+
+// // Test inquirer
+inquirer
+    .prompt(questions)
+    .catch((err) => console.error(err));
---
+++ src/Questions.js
@@ -0,0 +1,19 @@
+// Array of questions for inquirer to parse. 
+export default [
+    {
+        name: "fullName",
+        message: "Please enter the employee's full name\n",
+        type: "input",
+    },
+    {
+        name: "id",
+        message: "Please enter the employee's id\n",
+        type: "input",
+    },
+    {
+        name: "email",
+        message: "Please enter the employee's email\n",
+        type: "input",
+    },
+];
+
