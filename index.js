fa4051441dc3d717daa0720753748eaec0a6a655
Quin Eddy <qkeddy@gmail.com>
2022-04-10 09:48:12 -0400

    Built out questions module.

--- index.js
+++ index.js
@@ -4,27 +4,43 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-// const questions = require("./src/questions");
-const questions = require("./src/questions.json");
-
-
-// import questions from "./src/Questions.js";
+const Questions = require("./src/questions");
+const questions = require("./src/questions");
+// const questions = require("./src/questions.json");
 
 
 
-// Testing class plumbing
-const ryan = new Engineer("Ryan", 123, "ryan@me.com", "ryan@github");
-// const ginsu = new Intern("Ginsu", 789, "ginsu.eddy@gmail.com", "UofW");
-// const barbara = new Manager("Barbara", 456, "blmeelens@gmail.com", "212-888-7777");
+// Collect information for the manager
+function collectManagementInfo() {
+    const managementQuestions = new Questions(true);
+    // console.log(managementQuestion.getQuestions());
+    inquirer.prompt(managementQuestions.getQuestions()).catch((err) => console.error(err));
+}
 
-console.log(ryan.getGitHubUser());
-console.log(ryan.getId());
 
-// console.log(ginsu.getSchool());
-// console.log(ginsu.getId());
+// Collect information for the team
+function collectTeamInfo() {
+    const staffQuestions = new Questions(false);
+    // console.log(staffQuestion.getQuestions());
+    inquirer.prompt(staffQuestions.getQuestions()).catch((err) => console.error(err));
+}
 
-// console.log(barbara.getOfficeNumber());
-// console.log(barbara.getId());
 
-// // Test inquirer
-inquirer.prompt(questions).catch((err) => console.error(err));
+
+function init() {
+    // Testing class plumbing
+    const ryan = new Engineer("Ryan", 123, "ryan@me.com", "ryan@github");
+    const ginsu = new Intern("Ginsu", 789, "ginsu.eddy@gmail.com", "UofW");
+    const barbara = new Manager("Barbara", 456, "blmeelens@gmail.com", "212-888-7777");
+
+    console.log(`${ryan.getId()}  |  ${ryan.getGitHubUser()}`);
+    console.log(`${ginsu.getId()}  |  ${ginsu.getSchool()}`);
+    console.log(`${barbara.getId()}  |  ${barbara.getOfficeNumber()}`);
+
+    console.log("Welcome to the team profile builder");
+
+    collectManagementInfo();
+    collectTeamInfo();
+}
+
+init();
--- src/Questions.js
+++ src/Questions.js
@@ -1,23 +1,69 @@
 // Questions module
-const questions =
-    // Array of questions for inquirer to parse.
-    [
-        {
-            name: "fullName",
-            message: "Please enter the employee's full name\n",
-            type: "input",
-        },
-        {
-            name: "id",
-            message: "Please enter the employee's id\n",
-            type: "input",
-        },
-        {
-            name: "email",
-            message: "Please enter the employee's email\n",
-            type: "input",
-        },
-    ];
 
+// Pass into the questioning if it is a manager, engineer, or intern
+class Questions {
+    constructor(isManagement) {
+        // General questions
+        const generalQuestions = [
+            {
+                name: "fullName",
+                message: "Please enter the employee's full name\n",
+                type: "input",
+            },
+            {
+                name: "id",
+                message: "Please enter the employee's id\n",
+                type: "input",
+            },
+            {
+                name: "email",
+                message: "Please enter the employee's email\n",
+                type: "input",
+            },
+        ];
+        // Array of questions for a manager
+        if (isManagement) {
+            // Assemble one type of questions
+            const specificQuestions = [
+                {
+                    name: "managerPhone",
+                    message: "Please enter the managers's telephone number.\n",
+                    type: "input",
+                },
+            ];
+            // Assemble questions
+            const staffQuestions = [].concat(generalQuestions, specificQuestions);
+            this.staffQuestions = staffQuestions;
+        } else {
+            // Assemble a different type of questions
+            const employeeTypeQuestion = [
+                {
+                    name: "employeeTitle",
+                    message: "Please select employee type\n",
+                    choices: ["Engineer", "Intern"],
+                    type: "list",
+                },
+            ];
+            const specificQuestions = [
+                {
+                    name: "engineer",
+                    message: "Please enter the engineer's GitHub ID.\n",
+                    type: "input",
+                    when: (answers) => answers.employeeTitle === "Engineer",
+                },
+                {
+                    name: "intern",
+                    message: "Please enter the interns's school.\n",
+                    type: "input",
+                    when: (answers) => answers.employeeTitle === "Intern",
+                },
+            ];
+            // Assemble questions
+            const staffQuestions = [].concat(employeeTypeQuestion, generalQuestions, specificQuestions);
+            this.staffQuestions = staffQuestions;
+        }
+    }
+    getQuestions = () => this.staffQuestions;
+}
 
-module.exports = questions;
+module.exports = Questions;
